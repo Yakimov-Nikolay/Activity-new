72ced0ad2f0edeec0f4886cccee0f8813e173029
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-03-23 15:52:28 +0200

    Add files via upload

---
+++ Activity_new/src/src/main/java/activity_new/activity_new/model/entity/UserEntity.java
@@ -0,0 +1,149 @@
+package activity_new.activity_new.model.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private GenderEntity sex;
+    private Integer age;
+    private String password;
+    private String email;
+    private Integer phone;
+    private LocalDateTime birthday;
+    private String description;
+    private LocalDateTime createdPr;
+    private Set<RoleEntity> roles = new HashSet<>();
+
+
+    public UserEntity() {
+    }
+
+    @Column(nullable = false, unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    public UserEntity setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    @Column(nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    @OneToOne
+    public GenderEntity getSex() {
+        return sex;
+    }
+
+    public UserEntity setSex(GenderEntity sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    @Column(nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    @Column(nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @Column(unique = true, nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Column
+    public LocalDateTime getCreatedPr() {
+        return createdPr;
+    }
+
+    public UserEntity setCreatedPr(LocalDateTime createdPr) {
+        this.createdPr = createdPr;
+        return this;
+    }
+
+    @Column
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public UserEntity setPhone(Integer phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    @Column
+    public String getDescription() {
+        return description;
+    }
+
+    public UserEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Column
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public UserEntity setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+
+    @DecimalMin("10")
+    public Integer getAge() {
+        return age;
+    }
+
+    public UserEntity setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    public Set<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleEntity> roles) {
+        this.roles = roles;
+    }
+
+}
