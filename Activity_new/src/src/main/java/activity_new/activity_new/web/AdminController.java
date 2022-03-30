f1fe59c56268e45aa00ddead086363d977e98cb1
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-03-30 15:52:55 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/web/AdminController.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/web/AdminController.java
@@ -1,13 +1,26 @@
 package activity_new.activity_new.web;
 
+import activity_new.activity_new.service.StatisticsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
 
+
+    private final StatisticsService statisticsService;
+
+    public AdminController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
     @GetMapping("/admin")
-    public String admin(){
-        return "/admin_page";
+    public ModelAndView admin(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("stat", statisticsService.getStatistics());
+        modelAndView.setViewName("admin_page");
+
+        return modelAndView;
     }
 }
