f66808282697b12eab57be5283379b52d95d6405
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-04-03 21:10:54 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/repository/PictureRepository.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/repository/PictureRepository.java
@@ -3,6 +3,7 @@ package activity_new.activity_new.repository;
 import activity_new.activity_new.model.entity.PictureEntity;
 import activity_new.activity_new.model.view.PictureViewModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -10,5 +11,8 @@ import java.util.List;
 
 @Repository
 public interface PictureRepository extends JpaRepository<PictureEntity, Long> {
+
+    @Query("select p from PictureEntity p where p.activity.id = ?1")
+    List<PictureEntity> findAllByActivity_Id(Long id);
 
 }
