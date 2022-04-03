5569a5824eae3d55376ca7dee82436e822917fff
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-04-03 21:13:38 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/service/PictureService.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/service/PictureService.java
@@ -1,11 +1,16 @@
 package activity_new.activity_new.service;
 
 import activity_new.activity_new.model.binding.UploadPictureBindingModel;
+import activity_new.activity_new.model.entity.PictureEntity;
+import activity_new.activity_new.model.view.PictureViewModel;
+import org.springframework.data.jpa.repository.Query;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface PictureService {
 
     void uploadPicture(UploadPictureBindingModel uploadPictureBindingModel, String principalName, Long id) throws IOException;
 
+    List<PictureEntity> findAllByActivityId(Long id);
 }
