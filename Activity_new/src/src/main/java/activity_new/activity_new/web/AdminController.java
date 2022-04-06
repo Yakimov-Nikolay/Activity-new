93c6b6aabf532ccdb71b06b7ce1cb816360b8b3b
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-04-06 15:37:33 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/web/AdminController.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/web/AdminController.java
@@ -1,7 +1,10 @@
 package activity_new.activity_new.web;
 
+import activity_new.activity_new.repository.ActivityRepository;
+import activity_new.activity_new.repository.UserRepository;
 import activity_new.activity_new.service.StatisticsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,16 +13,23 @@ public class AdminController {
 
 
     private final StatisticsService statisticsService;
+    private final ActivityRepository activityRepository;
+    private final UserRepository userRepository;
 
-    public AdminController(StatisticsService statisticsService) {
+    public AdminController(StatisticsService statisticsService, ActivityRepository activityRepository, UserRepository userRepository) {
         this.statisticsService = statisticsService;
+        this.activityRepository = activityRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/admin")
-    public ModelAndView admin(){
+    public ModelAndView admin(Model model){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("stat", statisticsService.getStatistics());
         modelAndView.setViewName("admin_page");
+
+        model.addAttribute("count_of_activity", activityRepository.findAll());
+        model.addAttribute("count_of_users", userRepository.findAll());
 
         return modelAndView;
     }
