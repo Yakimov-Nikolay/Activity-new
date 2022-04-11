6d0dcb38e660e137fbcf5c9ef232838ef0dad853
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-04-11 10:54:58 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/web/HomeController.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/web/HomeController.java
@@ -1,15 +1,25 @@
 package activity_new.activity_new.web;
 
-import activity_new.activity_new.repository.ActivityRepository;
 import activity_new.activity_new.service.ActivityService;
+import activity_new.activity_new.service.QRCodeService;
+import com.google.zxing.WriterException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private QRCodeService qrCodeService;
 
     private final ActivityService activityService;
 
@@ -17,19 +27,36 @@ public class HomeController {
         this.activityService = activityService;
     }
 
-    @GetMapping("/")
-    public String index(){
+    @RequestMapping("/")
+    public String indexPage() {
+
         return "index";
     }
 
+    @PostMapping("/showQRCode")
+    public String showQRCode(String qrContent, Model model) {
+
+        model.addAttribute("qrCodeContent", "/generateQRCode?qrContent=" + qrContent);
+
+        return "show_qr_code";
+    }
+
+    @GetMapping("/generateQRCode")
+    public void generateQRCode(String qrContent, HttpServletResponse response) throws IOException, WriterException {
+        response.setContentType("image/png");
+        byte[] qrCode = qrCodeService.generateQRCode(qrContent, 500, 500);
+        OutputStream outputStream = response.getOutputStream();
+        outputStream.write(qrCode);
+    }
+
     @GetMapping("/home")
-    public String home(Model model, Principal principal){
+    public String home(Model model, Principal principal) {
         model.addAttribute("user_home", activityService.findActivityEntitiesByAuthorUsername(principal.getName()));
         return "home";
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about() {
         return "about";
     }
 
