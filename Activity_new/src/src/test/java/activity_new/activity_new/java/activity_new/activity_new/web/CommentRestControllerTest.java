76a51585826bae0c25aa34da94d625fb32ee0bfa
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-04-18 18:25:11 +0300

    Add files via upload

--- Activity_new/src/src/test/java/activity_new/activity_new/java/activity_new/activity_new/web/CommentRestControllerTest.java
+++ Activity_new/src/src/test/java/activity_new/activity_new/java/activity_new/activity_new/web/CommentRestControllerTest.java
@@ -1,17 +1,21 @@
 package activity_new.activity_new.web;
 
+import activity_new.activity_new.model.binding.AddCommentBindingModel;
 import activity_new.activity_new.model.entity.ActivityEntity;
 import activity_new.activity_new.model.entity.CommentEntity;
 import activity_new.activity_new.model.entity.UserEntity;
 import activity_new.activity_new.repository.ActivityRepository;
 import activity_new.activity_new.repository.CommentRepository;
 import activity_new.activity_new.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.text.MatchesPattern;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,17 +23,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WithMockUser("Ivan")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CommentRestControllerTest {
-    private final static String COMMENT_1 = "something";
-    private final static String COMMENT_2 = "something else";
+    private final static String COMMENT_1 = "Hey Spring is super!";
+    private final static String COMMENT_2 = "something else here";
 
     @Autowired
     private MockMvc mockMvc;
@@ -43,6 +48,8 @@ public class CommentRestControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
     private UserEntity testUser;
 
     @BeforeEach
@@ -64,29 +71,54 @@ public class CommentRestControllerTest {
     }
 
     @Test
-    void testGetComment() throws Exception {
-        long activityId = initActivity();
+    void testGetComments() throws Exception {
+        var activity = initComments(initActivity());
 
-        mockMvc.perform(get("/api" + activityId + "/comments"))
+        mockMvc.perform(get("/api" + activity.getId() + "/comments"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$.[0].message", is(COMMENT_1)))
                 .andExpect(jsonPath("$.[1].message", is(COMMENT_2)));
+    }
+
+    @Test
+    void testCreateComment() throws Exception {
+
+        AddCommentBindingModel testComment = new AddCommentBindingModel()
+                .setMessage(COMMENT_1);
+
+        var emptyActivity = initActivity();
+
+        mockMvc.perform(
+                post("/api/" + emptyActivity.getId() + "/comments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(testComment))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+        )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(header().string("Location", MatchesPattern.matchesPattern("/api" + emptyActivity.getId()+"/comments/\\d")))
+                .andExpect(jsonPath("$.message").value(is(COMMENT_1)));
 
     }
 
-    private long initActivity() {
+    private ActivityEntity initActivity() {
         ActivityEntity testActivity = new ActivityEntity();
         testActivity.setTitle("Test Activity");
 
-        testActivity = activityRepository.save(testActivity);
+        return activityRepository.save(testActivity);
+    }
+
+    private ActivityEntity initComments(ActivityEntity activity) {
+
 
         CommentEntity testComment1 = new CommentEntity();
         testComment1.setAuthor(testUser);
         testComment1.setCreated(LocalDateTime.now());
         testComment1.setContent(COMMENT_1);
         testComment1.setApproved(true);
-        testComment1.setActivity(testActivity);
+        testComment1.setActivity(activity);
 
 
         CommentEntity testComment2 = new CommentEntity();
@@ -94,11 +126,11 @@ public class CommentRestControllerTest {
         testComment2.setCreated(LocalDateTime.now());
         testComment2.setContent(COMMENT_2);
         testComment2.setApproved(true);
-        testComment2.setActivity(testActivity);
+        testComment2.setActivity(activity);
 
-        testActivity.setCommentEntityList(List.of(testComment1, testComment2));
+        activity.setCommentEntityList(List.of(testComment1, testComment2));
 
-        return activityRepository.save(testActivity).getId();
+        return activityRepository.save(activity);
     }
 
 }
