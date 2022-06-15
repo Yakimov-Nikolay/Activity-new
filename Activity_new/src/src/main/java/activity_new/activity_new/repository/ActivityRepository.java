047c653f29fabf3ce6a504b5c523bf97926b1a44
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-06-15 16:07:58 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/repository/ActivityRepository.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/repository/ActivityRepository.java
@@ -1,20 +1,20 @@
 package activity_new.activity_new.repository;
 
 import activity_new.activity_new.model.entity.ActivityEntity;
-import activity_new.activity_new.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<ActivityEntity,Long> {
+public interface ActivityRepository extends JpaRepository<ActivityEntity, Long> {
 
     @Query("select a from ActivityEntity a where a.author.username = ?1")
     List<ActivityEntity> findActivityEntitiesByAuthorUsername(String username);
 
-
     List<ActivityEntity> findAll();
+
+    ActivityEntity findAllByLikeVideoCounter(long counter);
 }
