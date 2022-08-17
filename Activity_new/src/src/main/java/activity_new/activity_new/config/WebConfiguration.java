c9e55d248e52d5cb9a68d75df86b96998e264bd7
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-08-17 16:57:40 +0300

    Add files via upload

--- Activity_new/src/src/main/java/activity_new/activity_new/config/WebConfiguration.java
+++ Activity_new/src/src/main/java/activity_new/activity_new/config/WebConfiguration.java
@@ -4,18 +4,23 @@ import activity_new.activity_new.web.Interceptors.StatisticsInterceptors;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final StatisticsInterceptors interceptors;
+    private final LocaleChangeInterceptor localeChangeInterceptor;
 
-    public WebConfiguration(StatisticsInterceptors interceptors) {
+    public WebConfiguration(StatisticsInterceptors interceptors, LocaleChangeInterceptor localeChangeInterceptor) {
         this.interceptors = interceptors;
+        this.localeChangeInterceptor = localeChangeInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptors);
+        registry.addInterceptor(localeChangeInterceptor);
     }
+
 }
